302cfedfdcdb3d860ca7dfc30b91b34196941522
Abinash <abinash@theabx.in>
2026-04-26 06:59:27 +0530

    Remove company numbering, add selection highlight

--- lisp/configs/company-config.el
+++ lisp/configs/company-config.el
@@ -6,12 +6,14 @@
   (setq company-idle-delay 0.2)
   (setq company-minimum-prefix-length 1)
   (setq company-selection-wrap-around t)
-  (setq company-show-numbers t)
+  (setq company-show-numbers nil)
   (setq company-tooltip-limit 8)
   (setq company-require-match nil)
   (setq company-global-modes '(not eshell-mode shell-mode term-mode vterm-mode))
   :config
-  (setq company-backends '(company-capf company-dabbrev-code company-keywords)))
+  (setq company-backends '(company-capf company-dabbrev-code company-keywords))
+  (setq company-tooltip-offset 0)
+  (set-face-attribute 'company-tooltip-selection nil :foreground 'unspecified :background "#3c3836"))
 
 (use-package yasnippet
   :ensure t
